85d7c1ab7bcad5106387e00a1d8225a5743c3f10
Ben Woskow <bwoskow@launchdarkly.com>
2019-05-03 11:23:04 -0700

    package rename

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View,
          Button, TextInput, Picker,
          Alert, Switch, Platform } from 'react-native';
-import LDClient from 'launchdarkly-react-native-client';
+import LDClient from 'launchdarkly-react-native-client-sdk';
 
 export default class App extends React.Component {
   constructor(props) {
