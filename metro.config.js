08e7195526228fb705f1c1aa3c779f463d922cf7
torchhound <5600929+torchhound@users.noreply.github.com>
2021-04-09 15:51:32 -0700

    Update to React Native 0.64

--- metro.config.js
+++ metro.config.js
@@ -10,7 +10,7 @@
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
       },
     }),
   },
