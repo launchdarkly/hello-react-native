aebb00e748433026c061f999e23e4903241168bf
Gavin Whelan <gwhelan@launchdarkly.com>
2022-04-22 09:32:35 -0500

    Simplified sample application for React Native 0.68.

---
+++ App.js
@@ -0,0 +1,63 @@
+/**
+ * LaunchDarkly Hello React Native App
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useState, useEffect } from 'react';
+import type {Node} from 'react';
+import { SafeAreaView, StatusBar, Text } from 'react-native';
+
+import LDClient from 'launchdarkly-react-native-client-sdk';
+
+const App: () => Node = () => {
+
+  const mobileKey = '';
+  const flagKey = 'my-boolean-flag';
+
+  const [client, setClient] = useState(null);
+  const [flagValue, setFlagValue] = useState(null);
+
+  async function evalFlag() {
+    let res = await client.boolVariation(flagKey, false);
+    if (res != flagValue) {
+      setFlagValue(res);
+    }
+  }
+
+  useEffect(() => {
+    async function initializeClient() {
+      let ldClient = new LDClient();
+      let config = { mobileKey: mobileKey, debugMode: true };
+      let user = { key: 'example-user-key' };
+      try {
+        await ldClient.configure(config, user);
+      } catch (err) {
+        console.log(err);
+      }
+      setClient(ldClient);
+    }
+
+    if (mobileKey === '') {
+      console.log('Please edit App.js to set mobileKey to your LaunchDarkly mobile key first');
+      return;
+    }
+
+    if (client == null) {
+      initializeClient();
+    } else {
+      evalFlag();
+    }
+  });
+
+  return (
+    <SafeAreaView>
+      <Text>
+        Feature flag '{flagKey}' is {JSON.stringify(flagValue)} for this user
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,13 @@
+# LaunchDarkly Sample React Native Application
+
+We've built a simple application that demonstrates how LaunchDarkly's SDK works. Below, you'll find the basic build procedure, but for more comprehensive instructions, you can visit your [Quickstart page](https://app.launchdarkly.com/quickstart#/).
+
+## Build instructions
+1. Open the `hello-react-native` directory
+2. Copy the mobile key from your account settings page from your LaunchDarkly dashboard into `App.js`.
+3. Run `npx yarn install` to download the dependencies for the application.
+4. For iOS, set up the native module dependencies by running `npx pod-install`.
+
+## Running
+
+To run the application use `npm run <android|ios>`
